360e963251fa10336102ccfeab60b976ccca9dbe
Aryan <aryan.motiondash@gmail.com>
2019-07-25 00:19:14 -0400

    Update 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,31 @@
-window.addEventListener("scroll", function(e) {
-  var nav = document.querySelector("nav");
-  if (document.documentElement.scrollTop >= 120) {
-    nav.classList.add("nav-colored");
-    nav.classList.remove("nav-transparent");
-  } else {
-    nav.classList.add("nav-transparent");
-    nav.classList.remove("nav-colored");
-  }
-});
+const navSlide = function() {
+  const burger = document.querySelector(".burger");
+  const navLinksLi = document.querySelectorAll(".nav-links li");
+  const navLinks = document.querySelectorAll(".nav-links");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", function() {
+    nav.classList.toggle("nav-active");
+    nav.classList.toggle("nav-colored");
+  });
+};
+
+const scrollToggle = () => {
+  window.addEventListener("scroll", function(e) {
+    const nav = document.querySelector("nav");
+    if (document.documentElement.scrollTop >= 120) {
+      nav.classList.add("nav-colored");
+      nav.classList.remove("nav-transparent");
+    } else {
+      nav.classList.add("nav-transparent");
+      nav.classList.remove("nav-colored");
+    }
+  });
+};
 
 function checkHeight() {
   console.log(document.documentElement.scrollTop);
 }
+
+navSlide();
+scrollToggle();
