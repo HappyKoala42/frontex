57c0753cd2de206037f907e94f8f08284b72fe84
Aryan <aryan.motiondash@gmail.com>
2019-07-25 19:29:40 -0400

    Update 7

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,12 +6,29 @@ const navSlide = () => {
 
   burger.addEventListener("click", function () {
     navlinks.classList.toggle("nav-active");
+    //burger animation
+    burger.classList.toggle("toggleBurger");
   });
+
+  for (let i = 0; i < navlinksli.length; i++) {
+    navlinksli[i].addEventListener("click", function () {
+      navlinks.classList.toggle("nav-active");
+      //burger animation
+      burger.classList.toggle("toggleBurger");
+    });
+  }
 };
 
 const scrollToggle = () => {
   window.addEventListener("scroll", function (e) {
     const nav = document.querySelector("nav");
+    const burger = document.querySelector(".burger");
+    const about = document.querySelector(".about-img");
+    const mission = document.querySelector(".mission-img");
+    const testimony = document.querySelector(".testimony-img");
+    const pricing = document.querySelector(".pricing-img");
+
+    // Navbar
     if (document.documentElement.scrollTop >= 120) {
       nav.classList.add("nav-colored");
       nav.classList.remove("nav-transparent");
@@ -19,16 +36,35 @@ const scrollToggle = () => {
       nav.classList.add("nav-transparent");
       nav.classList.remove("nav-colored");
     }
+    // Fade about section
+    if (document.documentElement.scrollTop >= 730) {
+      about.classList.add("fadeinAnim");
+    }
+    // Fade mission section
+    if (document.documentElement.scrollTop >= 1314) {
+      mission.classList.add("fadeinAnim");
+    }
+    // Fade testimony section
+    if (document.documentElement.scrollTop >= 2427) {
+      testimony.classList.add("fadeinAnim");
+    }
+    // Fade pricing section
+    if (document.documentElement.scrollTop >= 3275) {
+      pricing.classList.add("fadeinAnim");
+    }
+
   });
 };
 
 function checkHeight() {
-  console.log(document.documentElement.scrollTop);
+  window.addEventListener("scroll", function (e) {
+    console.log(document.documentElement.scrollTop);
+  });
 }
 
 navSlide();
 scrollToggle();
-
+checkHeight();
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles', 'public/scripts/particles.json', function () {
